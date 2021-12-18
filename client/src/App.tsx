444d13c2379b6df8eac75f2fa90717beff5f6f5a
adnan aslam <adnanaslam475@gmail.com>
2021-12-18 13:41:32 +0500

    aaxx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import axios from 'axios';
+import { Button } from '@mui/material';
+import { enums } from './constants';
 
 function App() {
+
+
+  const submitHandler = (e: any) => {
+    e.preventDefault();
+    axios.post('').then((res: any) => {
+      console.log(res)
+    }).catch(() => { console.log('error') })
+  }
+
   return (
     <div className="App">
-      sddddddddddddd
+      <form onSubmit={submitHandler}>
+        <Box
+          component="form"
+          sx={{
+            '& > :not(style)': { m: 3 },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          {enums.map(v => <div key={v.name}>
+            <TextField id={v.id} label={v.label} name={v.name} onChange={() => ''}
+              placeholder={v.placeholder} required variant="outlined" />
+          </div>
+          )}
+          <Button type='submit' variant='contained' color='primary' >Submit</Button>
+        </Box>
+      </form>
     </div>
   );
 }
---
+++ client/src/constants.js
@@ -0,0 +1,17 @@
+export const enums = [{
+    label: 'Project Name:', placeholder: 'Enter project Name',
+    type: 'text',
+    name: 'project_name',
+    id: 'project_name',
+},
+{
+    label: 'Company Name:', placeholder: 'Enter Company Name',
+    type: 'text',
+    name: 'company_name',
+    id: 'company_name',
+}, {
+    label: 'Contact Email:', placeholder: 'Enter contact email..',
+    type: 'email',
+    name: 'contact_email',
+    id: 'contact_email',
+}]
